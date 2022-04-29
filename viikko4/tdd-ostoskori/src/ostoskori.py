f6c4a3ba2cdeada3390dd60956b08a6b9c82c24b
Nella <nella.nieminen@gmail.com>
2022-04-29 11:29:50 +0300

    Testaa ostoskorin hintaa ja tavaroiden määrää

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,37 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        self.ostoskori = []
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+
+    def tavaroita_korissa(self):
+        tavaroita = 0
+        for ostos in self.ostoskori:
+            tavaroita += ostos.lukumaara
+        
+        return tavaroita
+
+    def hinta(self):
+        yhteishinta = 0
+        for ostos in self.ostoskori:
+            yhteishinta += ostos.hinta
+        return yhteishinta
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
